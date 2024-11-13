e34debb35c697ccf9bc9c5c3f25cadd83fac28a6
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2024-11-13 03:03:17 -0500

    prisma, register, login. jwt token create

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,32 @@
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+import prisma from '../lib/prisma.js';
+
+export const register = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        // Hash pw
+        const hashedPassword = await bcrypt.hash(password, 10)
+        console.log(hashedPassword)
+
+        // Create new user and save to DB
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+            }
+        })
+        console.log(newUser)
+        res.status(201).json({message: "User created successfully"})
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "Failed to create user"})
+    }  
+} 
+
 export const login = async (req, res) => {
     const { username, password } = req.body;
 
